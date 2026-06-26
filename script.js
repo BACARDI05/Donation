7ff3cd41bf9e06237d6c40bfc161f2199cf371c3
Xero <sahnurana12345678@gmail.com>
2026-06-27 02:02:05 +0530

    Update index.html, script.js, and style.css files to add new features and improve styling.

---
+++ script.js
@@ -0,0 +1,9 @@
+function openTab(tabName){
+    document.querySelectorAll(".page").forEach(page=>{
+        page.classList.remove("active");});
+    document.getElementById(tabName).classList.add("active");
+    document.querySelectorAll(".tab").forEach(btn=>{
+        btn.classList.remove("active");
+    });
+    event.currentTarget.classList.add("active");
+}
