6143e2ef0ed6635bb70b46b7386ae8c068a134ac
Xero <sahnurana12345678@gmail.com>
2026-06-27 02:22:50 +0530

    Fixing Download Scripts heading

--- script.js
+++ script.js
@@ -1,8 +1,15 @@
 function openTab(tabName){
-    document.querySelectorAll(".page").forEach(page=>{
-        page.classList.remove("active");});
-    document.getElementById(tabName).classList.add("active");
-    document.querySelectorAll(".tab").forEach(btn=>{
+    document
+    .querySelectorAll(".page")
+    .forEach(page => {
+        page.classList.remove("active");
+    });
+    document
+    .getElementById(tabName)
+    .classList.add("active");
+    document
+    .querySelectorAll(".tab")
+    .forEach(btn => {
         btn.classList.remove("active");
     });
     event.currentTarget.classList.add("active");
