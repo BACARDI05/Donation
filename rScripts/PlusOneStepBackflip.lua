6e5dda774b47ed2c74aa183ce2581f493c846442
Xero <99786322+BACARDI05@users.noreply.github.com>
2026-06-27 23:55:16 +0530

    Revise script usage disclaimer
    Updated warning message for script usage.

--- rScripts/PlusOneStepBackflip.lua
+++ rScripts/PlusOneStepBackflip.lua
@@ -1,5 +1,5 @@
 --[[
     Use scripts on your own risk, We are not responsible for any bans or issues that may arise from using these scripts.
-    Therefore we are here to resolve script related issues.
+    Feel free to ask script related issues.
 ]]
 loadstring(game:HttpGet("https://obfuscatorhub.onrender.com/api/2jlht3dd"))()
